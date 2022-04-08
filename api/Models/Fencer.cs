eb72b2baa193fd9154d3bcedcb73ae11cfe4ece3
jamesw98 <xeekatar@gmail.com>
2022-04-07 23:47:09 -0400

    added gender to fencer model

--- api/Models/Fencer.cs
+++ api/Models/Fencer.cs
@@ -5,5 +5,6 @@ public class Fencer
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string School { get; set; }
+    public string Gender { get; set; }
     public int TournamentsAttended { get; set; }
 }
