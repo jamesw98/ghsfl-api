1fb623e157a0f2681bbc2647f6b7894a396266af
jamesw98 <xeekatar@gmail.com>
2022-04-12 22:44:25 -0400

    added getfencersforschoolforround, returns all fencers submitted for a specific school for a specific round

--- api/Repositories/SchoolRepository.cs
+++ api/Repositories/SchoolRepository.cs
@@ -12,6 +12,41 @@ public class SchoolRepository
         DbConnection = new SqliteConnection("Data source=ghsfl_dev.db");
         DbConnection.Open();
     }
+
+    public async Task<List<Fencer>> GetFencersForSchoolForRound(string schoolName, int round)
+    {
+        List<Fencer> fencers = new List<Fencer>();
+    
+        var command = DbConnection.CreateCommand();
+        command.CommandText =
+            @"
+                SELECT 
+                    f.firstname,
+                    f.lastname,
+                    f.gender
+                FROM FencerRounds fr
+                    INNER JOIN Fencers f ON fr.fencer_id = f.id
+                WHERE
+                    fr.round = @round AND 
+                    f.school = @school COLLATE NOCASE
+            ";
+        command.Parameters.AddWithValue("round", round);
+        command.Parameters.AddWithValue("school", schoolName);
+        
+        var reader = await command.ExecuteReaderAsync();
+        
+        while (reader.Read())
+        {
+            fencers.Add(new Fencer
+            {
+                FirstName = reader.GetString(0),
+                LastName = reader.GetString(1),
+                Gender = reader.GetString(2)[0] == 'F' ? "Female" : "Male"
+            });    
+        }
+
+        return fencers;
+    }
     
     /// <summary>
     /// returns all fencers that have been submitted in a roster for the given school
